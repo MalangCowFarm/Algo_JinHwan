2183efa41b6aef65b0b8a1fc986e5f00fbff8b14
redjinh <ghdtka1218@gmail.com>
2023-04-15 15:49:12 +0900

    xmfflf

--- week 11/부분합_1806(BOJ)/sol.py
+++ week 11/부분합_1806(BOJ)/sol.py
@@ -33,7 +33,42 @@ if res == 1e19:
 else:
     print(res)
 
+#_____________________________________________________________________________
+# 틀린 식
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+
+s = 0
+e = 0
+res = 1e19
+result = arr[0]
+cnt = 2
 
 
 
+if sum(arr) == M:
+    res = N
+    for i in arr:
+        if i >= M:
+            res = 1
+else:
+    while s < N-1 and e < N-1:
+        if result < M:
+            e += 1
+            result += arr[e]
+            cnt += 1
+        elif result >= M:
+            result -= arr[s]
+            s += 1
+            cnt -= 1
+            if res > cnt:
+                res = cnt
+
+
+
+
+if res == 1e19:
+    print(0)
+else:
+    print(res)
 
