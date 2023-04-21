91dc3310b103e1d4e8956c36ffd0ebf16d718c01
홍진환 <ghdtka1218@gmail.com>
2023-04-21 17:55:59 +0900

    20230421

--- week 12/7576/sol.py
+++ week 12/7576/sol.py
@@ -1,45 +1,28 @@
 import sys
 sys.stdin = open("input.txt")
 from collections import deque
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-def bfs(x, y, cnt):
-    global result
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < M and 0 <= ny < N and visited[nx][ny] == 0:
-            arr[nx][ny] = 1
-            print(nx, ny)
-            print(visited)
 
 
-def bfs(x, y):
-    q = deque([x, y])
-
-    while q:
-        x, y = q.popleft()
-        print(x, y, end = '')
+def bfs(V):
+    visited[V] = 0
+    while queue:
+        V = queue.pop(0)
+        print(V, end = ' ')
         for i in range(1, N+1):
-            if not visited[i]
-
-
-
-        print(arr)
-
-
-
-
+            if visited[i] == 1 and arr[V][i] == 1:
+                queue.append(i)
+                visited[i] = 0
 
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(M)]
 
 visited = [[0 for _ in range(N)] for _ in range(M)]
+queue = deque()
 
 result = 0
 for i in range(M):
     for j in range(N):
         if arr[i][j] == 1:
-            bfs(i, j, 0)
+            bfs()
 
