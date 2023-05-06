ba25cb5d155c38a13fa5054b9d48378e5fd8e6f6
redjinh <ghdtka1218@gmail.com>
2023-05-06 14:15:14 +0900

    2023

--- week 14/선 긋기_2170(BOJ)/sol.py
+++ week 14/선 긋기_2170(BOJ)/sol.py
@@ -3,7 +3,7 @@ sys.stdin = open("input.txt")
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 arr.sort(key = lambda x: x[0])
-
+print(arr)
 start, end = arr[0][0], arr[0][1]
 result = 0
 if len(arr) > 1:
